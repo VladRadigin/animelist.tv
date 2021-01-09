1ea2f0f4f49d3f51e29a30458b79afca6542833a
VladRadygin <angry.gamer.comi@gmail.com>
2021-01-09 20:13:30 +0600

    Minor editing of the genre list model

--- myList/admin.py
+++ myList/admin.py
@@ -3,7 +3,7 @@ from .models import Bb
 from .models import Rubric
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'views', 'published')
+    list_display = ('title', 'content', 'views', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content', )
 
