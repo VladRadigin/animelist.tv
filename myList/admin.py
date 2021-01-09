81de53d08648e03dac04ecf77a47906f5476aa8e
VladRadygin <angry.gamer.comi@gmail.com>
2021-01-09 18:25:40 +0600

    Genre model creation

--- myList/admin.py
+++ myList/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Bb
+from .models import Rubric
 
 class BbAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'views', 'published')
@@ -7,3 +8,4 @@ class BbAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content', )
 
 admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
--- myList/models.py
+++ myList/models.py
@@ -5,8 +5,18 @@ class Bb(models.Model):
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     views = models.FloatField(null=True, blank=True, verbose_name='Просмотры')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Жанры')
 
     class Meta:
         verbose_name_plural = 'Список аниме'
         verbose_name = 'Аниме'
         ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+
+    class Meta:
+        verbose_name_plural = 'Жанры'
+        verbose_name = 'Жанр'
+        ordering = ['name']
