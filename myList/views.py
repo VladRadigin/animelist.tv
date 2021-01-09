4330716255f0e77c9e56a25f8e2bafd14a54ed29
VladRadygin <angry.gamer.comi@gmail.com>
2021-01-09 11:11:53 +0600

    Adding the Anime Job Subsystem to the Default Database

--- myList/views.py
+++ myList/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Bb
 from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    template = loader.get_template('myList/index.html')
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'myList/index.html', {'bbs': bbs})
