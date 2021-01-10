359bc00bc604a63001138aa7cdf5c107a615abee
VladRadygin <angry.gamer.comi@gmail.com>
2021-01-10 12:47:51 +0600

    Adding a new form for view controller

--- myList/views.py
+++ myList/views.py
@@ -4,6 +4,8 @@ from .models import Bb
 from django.template import loader
 from django.shortcuts import render
 from .models import Rubric
+from django.views.generic.edit import CreateView
+from .forms import BbForm
 
 def index(request):
     bbs = Bb.objects.all()
@@ -17,3 +19,13 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'myList/by_rubric.html', context)
+
+def BbCreateView(CreateView):
+    template_name = 'myList/create.html'
+    form_class = BbForm
+    success_url = '/myList/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
